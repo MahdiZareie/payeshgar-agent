a07340a2aa86426387f3ec119d02c4f1b5bc10c4
Mahdi Zareie <mahdi.elf@gmail.com>
2020-07-14 12:32:54 +0430

    Consider URL valid without path

--- utils.py
+++ utils.py
@@ -2,8 +2,12 @@ from urllib.parse import urlparse
 
 
 def validate_url(url: str):
+    """
+    Validate given url, and return True if the url is valid
+    :return: True for valid, False for invalid
+    """
     try:
         result = urlparse(url)
-        return all([result.scheme, result.netloc, result.path])
+        return all([result.scheme, result.netloc])
     except:
         return False
