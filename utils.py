0c588fd6f7ddfdca9e479b02662afc8471131dd5
Mahdi Zareie <mahdi.elf@gmail.com>
2020-07-15 02:24:36 +0430

    Create a function to read from dict or raise a custom exception

--- utils.py
+++ utils.py
@@ -11,3 +11,14 @@ def validate_url(url: str):
         return all([result.scheme, result.netloc])
     except:
         return False
+
+
+def get_key_or_raise(dictionary: dict, key: str, exp: Exception):
+    """
+    get the key from dictionary or raise the exp
+
+    """
+    try:
+        return dictionary[key]
+    except KeyError:
+        raise exp
