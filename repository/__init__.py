70c37fbed9331553ed89dac75bb43669d58b636a
Mahdi Zareie <mahdi.elf@gmail.com>
2020-07-15 02:37:56 +0430

    Introduce the concept of repository

---
+++ repository/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package represents the concept of Repository, each module in this package contains a repository definition for
+an entity in the server API.
+the repository isolates server API details and provides a simple CRUD-like interface to the rest of objects
+"""
