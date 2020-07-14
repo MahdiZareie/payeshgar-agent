8df29b68f900dab244339eba1c51324cd08ac958
Mahdi Zareie <mahdi.elf@gmail.com>
2020-07-14 12:19:38 +0430

    Create separate function to read config

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import sys
 import time
-from config import AgentConfig
+from config import AgentConfig, ValidationError
 from application import Application
+import os
+
+
+def get_config() -> AgentConfig:
+    try:
+        config_file = os.getenv("PAYESHGAR_CONFIG_FILE")
+        if config_file is None:
+            print("Please set PAYESHGAR_CONFIG_FILE environment variable to a valid Payeshgar config file")
+            exit(1)
+        return AgentConfig.from_file(config_file)
+    except ValidationError as err:
+        print("Config file content is not valid for because: {}".format(
+            "\n".join(["{}: {}".format(key, value) for key, value in err.detail.items()])
+        ))
+    except Exception as exp:
+        print(str(exp))
+    exit(1)
 
 
 def main():
-    config_file = sys.argv[1]
-    agent_config = AgentConfig.from_file(config_file)
+    agent_config = get_config()
     apps = []
     for server in agent_config.servers:
         app = Application(agent_info=agent_config.agent_info, server_info=server)
