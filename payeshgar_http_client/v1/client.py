318c597c1f112f36fadb012ba52b720f02da39ce
Mahdi Zareie <mahdi.elf@gmail.com>
2020-07-22 00:06:28 +0430

    Implement pagination in API client

--- payeshgar_http_client/v1/client.py
+++ payeshgar_http_client/v1/client.py
@@ -1,4 +1,5 @@
 import json
+import time
 from datetime import datetime
 from typing import Optional, List
 
@@ -12,6 +13,7 @@ class PayeshgarServerHTTPClient:
         self.base_url = base_url.strip("/") + v1_prefix
         self.username, self.password = username, password
         self._token = None
+        self._api_timeout = 5
         self.session = self.initialize_session()
 
     def initialize_session(self):
@@ -35,15 +37,26 @@ class PayeshgarServerHTTPClient:
 
     def introduce_agent(self, agent: models.AgentDTO):
         url = self._make_url("monitoring/agents")
-        response = self.session.post(url, data=agent.as_json(), timeout=5)
+        response = self.session.post(url, data=agent.as_json(), timeout=self._api_timeout)
         if response.status_code == 400:
             raise Exception("SERVER ERROR: {}".format(str(response.json())))
         if response in (200, 201):
             return
 
+    def _get_request(self, relative_url: str, params: dict):
+        url = self._make_url(relative_url)
+        while url is not None:
+
+            response = self.session.get(url, params=params, timeout=self._api_timeout)
+            if response.status_code != 200:
+                raise Exception("SERVER ERROR: {}".format(response.content))
+            data = response.json()
+            for item in data['results']:
+                yield item
+            url = data.get('next')
+
     def get_inspections(self, groups: Optional[list] = None, before: Optional[datetime] = None,
                         after: Optional[datetime] = None):
-        url = self._make_url("inspecting/inspections")
         query_params = {}
         if groups:
             query_params['groups'] = groups
@@ -51,10 +64,7 @@ class PayeshgarServerHTTPClient:
             query_params['after'] = after
         if before:
             query_params['before'] = before
-        response = self.session.get(url, params=query_params, timeout=5)
-        if response.status_code == 200:
-            return response.json()  # <-- FIXME
-        raise Exception("SERVER ERROR: {}".format(response.content))
+        return self._get_request("inspecting/inspections", query_params)
 
     def submit_results(self, results: List[dict]):
         url = self._make_url("inspecting/inspection-results?validate=1")  # <-- FIXME remove validate later
@@ -64,9 +74,17 @@ class PayeshgarServerHTTPClient:
         raise Exception("SERVER ERROR: {}".format(response.content))
 
     def get_endpoints(self, groups: Optional[List[str]]):
-        url = self._make_url('monitoring/endpoints')
-        params = dict(groups=groups) if groups else {}
-        response = self.session.get(url, params=params)
-        if response.status_code == 200:
-            return response.json()  # <-- FIXME
-        raise Exception("SERVER ERROR: {}".format(response.content))
+        query_params = dict(groups=groups) if groups else {}
+        return self._get_request('monitoring/endpoints', query_params)
+
+
+c = PayeshgarServerHTTPClient('http://localhost:8000', 'apollo', '1')
+for i in c.get_endpoints([]):
+    print(i)
+print("F")
+
+before = time.time()
+for i in c.get_inspections():
+    aft = time.time()
+    print(round(aft - before, 2))
+    before = aft
